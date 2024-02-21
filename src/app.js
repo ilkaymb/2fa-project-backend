8022fc522a2c907029ac5cf460e660d70ddb2da1
ilkaymb <ilkaymb@hotmail.com>
2024-02-22 01:59:30 +0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocs = require("./swaggerOptions");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Sadece bu kaynaktan gelen isteklere izin ver
+  })
+);
+
 app.use(express.json());
 
 // Swagger UI'ı '/api-docs' path'inde kullanılabilir hale getirin
@@ -12,7 +21,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 // Diğer route'larınız...
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Server running on port ${"http://localhost:" + PORT}`)
 );
