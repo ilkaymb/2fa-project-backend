69085b071d523d926a5472839343dd7f2b1afe6c
ilkaymb <ilkaymb@hotmail.com>
2024-02-22 18:01:59 +0300

    Add nodemailer configuration

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 module.exports = {
   secretKey: process.env.SECRET_KEY,
+  transporter: nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  }),
 };
