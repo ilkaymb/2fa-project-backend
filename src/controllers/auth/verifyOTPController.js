872a42df65c5b1ac522a306e7c4b1a1d9ca7f34f
ilkaymb <ilkaymb@hotmail.com>
2024-02-26 07:16:35 +0300

    Refactor OTP verification logic

--- src/controllers/auth/verifyOTPController.js
+++ src/controllers/auth/verifyOTPController.js
@@ -6,58 +6,44 @@ const jwt = require("jsonwebtoken");
 const { keys, mail } = require("../../config");
 
 exports.verifyOTP = async (req, res) => {
-  const { otp, STEP1_TOKEN } = req.body;
-  let username,
-    exp = "";
+  const { otp } = req.body;
+  const { user, exp } = req.user;
 
-  jwt.verify(STEP1_TOKEN, keys.step1_key, (err, decoded) => {
-    if (err) {
-      return res
-        .status(400)
-        .send(
-          err.name === "TokenExpiredError"
-            ? "Token süresi doldu."
-            : "Token doğrulanamadı."
-        );
-    } else {
-      username = decoded.user;
-      exp = decoded.exp;
-    }
+  console.log("user", user);
+  console.log("exp", exp);
+  const userProfile = Users.getUser(user);
+  if (!userProfile) {
+    return res.status(404).send("User not found");
+  }
 
-    const user = Users.getUser(username);
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (currentTime > exp) {
+    return res.status(400).send("OTP expired");
+  }
 
-    const currentTime = Math.floor(Date.now() / 1000);
-    if (currentTime > exp) {
-      return res.status(400).send("OTP expired");
-    }
-
-    const verified = speakeasy.totp.verify({
-      secret: user.secret,
-      encoding: "base32",
-      step: 600,
-      token: otp,
-      window: 1,
-    });
-
-    if (verified) {
-      // OTP doğrulama başarılı, STEP2_TOKEN oluştur
-      const STEP2_TOKEN = jwt.sign(
-        { username: user.username },
-        keys.step2_key,
-        { expiresIn: "1h" }
-      );
-
-      // Doğrulama başarılı ve STEP2_TOKEN ile yanıt gönder
-      res.status(200).json({
-        message: "OTP verified successfully",
-        token: STEP2_TOKEN,
-      });
-    } else {
-      // Doğrulama başarısız
-      res.status(400).send("Invalid OTP");
-    }
+  const verified = speakeasy.totp.verify({
+    secret: userProfile.secret,
+    encoding: "base32",
+    step: 600,
+    token: otp,
+    window: 1,
   });
+
+  if (verified) {
+    // OTP doğrulama başarılı, STEP2_TOKEN oluştur
+    const STEP2_TOKEN = jwt.sign(
+      { username: userProfile.username, type: "STEP2-TOKEN" },
+      keys.step2_key,
+      { expiresIn: "1h" }
+    );
+
+    // Doğrulama başarılı ve STEP2_TOKEN ile yanıt gönder
+    res.status(200).json({
+      message: "OTP verified successfully",
+      token: STEP2_TOKEN,
+    });
+  } else {
+    // Doğrulama başarısız
+    res.status(400).send("Invalid OTP");
+  }
 };
