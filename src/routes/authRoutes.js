5aef2292f95153205bff0d5f7b691795a8cbc22a
ilkaymb <ilkaymb@hotmail.com>
2024-02-26 07:17:57 +0300

    Add verifyUser endpoint and update verifyOTP endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const verifyToken = require("../middlewares/verifyToken"); // verifyToken middleware'inin yolu
+
 const { register } = require("../controllers/auth/registerController");
 const { login } = require("../controllers/auth/loginController");
 const { verifyOTP } = require("../controllers/auth/verifyOTPController");
+const { verifyUser } = require("../controllers/auth/verifyUserController");
 
 const router = express.Router();
 
@@ -73,12 +76,12 @@ router.post("/login", login);
  *             type: object
  *             required:
  *               - otp
- *               - jwtToken
+ *               - token
  *             properties:
  *               otp:
  *                 type: string
- *                 description: The otp sent to the user's email .
- *               jwtToken:
+ *                 description: The otp sent to the user's email.
+ *               token:
  *                 type: string
  *                 description: The jwtToken trying to log in.
  *     responses:
@@ -89,6 +92,48 @@ router.post("/login", login);
  *       404:
  *         description: User not found.
  */
-router.post("/verifyOTP", verifyOTP);
+router.post("/verifyOTP", verifyToken, verifyOTP);
+
+/**
+ * @swagger
+ * /api/verifyUser:
+ *   post:
+ *     summary: Verify user's JWT token
+ *     description: Verifies the JWT token provided by the user to ensure they are authenticated.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - token
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 description: The JWT token provided by the user for verification.
+ *     responses:
+ *       200:
+ *         description: User verified successfully. Returns user information.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     username:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *       400:
+ *         description: Token validation failed or token is expired.
+ *       403:
+ *         description: Token not provided.
+ */
+router.post("/verifyUser", verifyToken, verifyUser);
 
 module.exports = router;
